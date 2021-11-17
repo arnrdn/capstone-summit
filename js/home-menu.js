a40dfd1e6cc36523fb8d73a224218763f22d2d40
rdnrn <rodina.arina00@gmail.com>
2021-11-17 16:41:30 +0300

    Finish styling

--- js/home-menu.js
+++ js/home-menu.js
@@ -1,7 +1,13 @@
 const menuIcon = document.querySelector('.hamburger');
 const hamburger = document.querySelector('.header-wrapper');
+const body = document.querySelector('body');
 
 
 menuIcon.addEventListener('click', () => {
   hamburger.classList.toggle('change');
+  if (hamburger.classList.contains('change') === true) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
 })
